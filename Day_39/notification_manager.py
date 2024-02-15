991c926a6cb777443a9916f5fdb03b316f14b914
Dominika Goik <141367499+DomGoi@users.noreply.github.com>
2024-02-15 18:34:26 +0100

    Update notification_manager.py

--- Day_39/notification_manager.py
+++ Day_39/notification_manager.py
@@ -3,9 +3,9 @@ import os
 class NotificationManager:
     def __init__(self):
         self.account_sid="ACd9523a5cc270c13c323c29003afb676a"
-        self.auth_token="574e7a21e1e3d7d969575b18822cf6f7"
-        self.nr_twilio="+17163550664"
-        self.my_nr="+48607723330"
+        self.auth_token=os.environ["auth_token"]
+        self.nr_twilio=os.environ["nr_Twilio"]
+        self.my_nr=os.environ["my_nr"]
 
     def send_SMS(self):
         client = Client(self.account_sid, self.auth_token)
