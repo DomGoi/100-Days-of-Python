1f680ff5d2c3200d9e74e8eed972a79a1c79b30f
Dominika Goik <141367499+DomGoi@users.noreply.github.com>
2024-03-04 13:54:15 +0100

    Update main.py

--- Day_51/main.py
+++ Day_51/main.py
@@ -7,5 +7,6 @@ URL_1="https://twitter.com/i/flow/signup"
 
 internet=InternetSpeedTwitterBot(PROMISED_DOWN_MIN,PROMISED_UP_MIN)
 down,up, provider=internet.get_internet_speed()
-internet.tweet_at_provider(down,up,provider)
+if PROMISED_DOWN_MIN>int(down) or PROMISED_UP_MIN>int(up):
+    internet.tweet_at_provider(down,up,provider)
 
